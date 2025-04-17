21dcaab79949246a21db0a4cdcf6789adaa0606b
aggrey wanamamah <aggreywanamamah008@gmail.com>
2025-04-17 22:48:23 +0100

    createsi index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 const titleCased = () => {
   return tutorials
+}
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+
+    const titleCasedWords = words.map(word => {
+      if (word.length === 0) return word;
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+    
+    return titleCasedWords.join(' ');
+  });
 }
